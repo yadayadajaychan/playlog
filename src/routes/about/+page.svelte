3e56d971a36c5958b4b10ad698d6dbfef30075a9
Ethan Cheng <ethan@nijika.org>
2025-05-23 23:52:12 -0700

    add version info to frontend at build time

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,23 @@
+<script>
+	const programVersion = __APP_VERSION__;
+</script>
+
 <div class="p-4 place-self-start">
+	<p>
+	Playlog version {programVersion}
+	</p>
+	<br>
+
+	<p>
+	Copyright (C) 2025 Ethan Cheng &lt;ethan@nijika.org&gt;<br>
+	License: GNU AGPLv3+ &lt;http://gnu.org/licenses/agpl.html&gt;<br>
+	This is free software: you are free to change and redistribute it.<br>
+	There is NO WARRANTY, to the extent permitted by law.<br>
+	</p>
+	<br>
+
+	<p>
 	Source code for this website can be found at
 	<a class="underline text-blue-500 whitespace-nowrap" href="https://github.com/yadayadajaychan/playlog">https://github.com/yadayadajaychan/playlog</a>
+	</p>
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
+
+const programVersion = fs.readFileSync('VERSION', 'utf-8').trim()
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-	// TODO remove later
 	server: {
 		proxy: {
 			'/api': 'http://localhost:5000'
 		}
-	}
+	},
+	define: {
+		__APP_VERSION__: JSON.stringify(programVersion),
+	},
 });
