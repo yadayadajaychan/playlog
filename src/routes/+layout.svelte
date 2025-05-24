ce1d5f6482ce5d17bb2d5a2969cdd68f9f7f412e
Ethan Cheng <ethan@nijika.org>
2025-05-23 22:02:41 -0700

    frontend: add nav bar and /about/ page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,15 @@
 	let { children } = $props();
 </script>
 
+<nav>
+<ul class="pt-4 pl-4 pr-4 space-x-2 flex flex-row text-blue-500 underline">
+	<li>
+		<a href="/">Playlog</a>
+	</li>
+	<li>
+		<a href="/about/">About</a>
+	</li>
+</ul>
+</nav>
+
 {@render children()}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<div class="p-4 place-self-start">
+	Source code for this website can be found at
+	<a class="underline text-blue-500 whitespace-nowrap" href="https://github.com/yadayadajaychan/playlog">https://github.com/yadayadajaychan/playlog</a>
+</div>
