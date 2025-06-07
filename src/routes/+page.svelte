52bdeaa38549d2bc4bf740a3637cb312b6c06cec
Ethan Cheng <ethan@nijika.org>
2025-06-06 21:45:25 -0700

    frontend: display dx rating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -204,7 +204,8 @@
 				<strong>Fast:</strong> {entry.PlayInfo.FastCount} |
 				<strong>Late:</strong> {entry.PlayInfo.LateCount}<br>
 				<strong>Max Combo:</strong> {entry.PlayInfo.MaxCombo}/{entry.PlayInfo.TotalCombo}<br>
-				<strong>Rating:</strong> {entry.PlayInfo.AfterRating} (+{entry.PlayInfo.AfterRating-entry.PlayInfo.BeforeRating})<br>
+				<strong>Total Rating:</strong> {entry.PlayInfo.AfterRating} (+{entry.PlayInfo.AfterRating-entry.PlayInfo.BeforeRating})<br>
+				<strong>DX Rating:</strong> {entry.DxRatingGen3}<br>
 				<strong>Prev. Best:</strong> {entry.PreviousBestScore/10000}% ({scoreDiff(entry.PreviousBestScore, entry.PlayInfo.Score)}%)<br>
 				<strong>Track:</strong> {entry.PlayInfo.Track}<br>
 				<strong>Played with:</strong>
